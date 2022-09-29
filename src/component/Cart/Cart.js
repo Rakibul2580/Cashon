89c4c42a0fde7f4a099fccba3899e5e0e4b81a34
Rakibul2580 <rh565153@gmail.com>
2022-09-29 15:44:06 +0300

    add AOS

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,13 +1,17 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 
 const Cart = ({data, notifyStime}) => {
     const {name, time, picture} = data
+    useEffect(()=>{
+        AOS.init();
+      },[])
     return (
         <div>
-            <div className="card w-full h-full shadow-lg bg-base-100 shadow-indigo-500/40">
+            <div className="card w-full h-full shadow-lg bg-base-100 shadow-indigo-500/40" data-aos="zoom-in-up">
             <img src={picture} alt="Shoes" className='h-[382px]' />
             <div className="card-body text-white bg-gradient-to-r from-sky-400 to-indigo-500">
                 <h2 className="card-title block">{name}</h2>
