a3b8a5b20b6f38e382dbcacbf7ef2633d6adf68d
Rakibul2580 <rh565153@gmail.com>
2022-09-29 14:05:17 +0300

    update Cart

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,4 +1,3 @@
-import { data } from 'autoprefixer';
 import React from 'react';
 
 
@@ -8,13 +7,12 @@ const Cart = ({data, notifyStime}) => {
     const {name, time, picture} = data
     return (
         <div>
-            <div className="card w-full h-full shadow-lg bg-base-100 shadow-indigo-500/40 bg-white">
+            <div className="card w-full h-full shadow-lg bg-base-100 shadow-indigo-500/40">
             <img src={picture} alt="Shoes" className='h-[382px]' />
             <div className="card-body text-white bg-gradient-to-r from-sky-400 to-indigo-500">
                 <h2 className="card-title block">{name}</h2>
                 <p>Time required: {time} Minutes</p>
                 <div className="card-actions justify-center">
-                {/* <button onClick={notify} className="btn bg-transparent text-white w-45">Add Now</button><ToastContainer /> */}
                 <button onClick={()=>notifyStime(time)} className="btn bg-transparent text-white w-full">Add Now</button>
                 </div>
             </div>
