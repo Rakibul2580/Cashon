aee319bb78bdb47d943f2b6f0eccb13b4307dddc
Rakibul2580 <rh565153@gmail.com>
2022-09-29 19:40:59 +0300

    responsiv update

--- src/component/Body/Body.js
+++ src/component/Body/Body.js
@@ -53,15 +53,15 @@ const Body = () => {
             <h1 className='text-left text-blue-900 md:text-5xl md:font-bold text-4xl font-semibold '><FontAwesomeIcon icon={faPersonWalking}></FontAwesomeIcon> Morning work</h1>
             <p className='text-2xl my-10 font-semibold text-white bg-gradient-to-r from-sky-400 to-indigo-400 p-5 w-full rounded-lg'>Select today’s exercise</p>
             <div className='block md:flex'>
-                <div className='w-12/12 md:w-8/12 lg:9/12'>
-                <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+                <div className='w-74%'>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
                     {
                         datas.map(data => <Cart data={data} notifyStime={notifyStime} key={data._id}></Cart>)
                     }
                 </div>
                 <Qcollapse></Qcollapse>
                 </div>
-                <div className='w-12/12 md:w-4/12 lg:3/12 px-2 md:px-3 shadow-lg h-full py-8 ml-0 md:ml-3 md:sticky top-0'>
+                <div className='w-24% px-2 md:px-3 shadow-lg h-full py-8 ml-0 md:ml-3 md:sticky top-0'>
                     <Myself></Myself>
                     <Break setValue={setValue}></Break>
                     <Details localData={localData} setExeTime={setExeTime}></Details>
